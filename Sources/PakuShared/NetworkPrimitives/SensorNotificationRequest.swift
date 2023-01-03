0734debc722d7207d01e07a950cb1030f47d2dfb
Kyle Bashour <kylebshr@me.com>
2023-01-03 14:03:29 -0800

    Public

--- Sources/PakuShared/NetworkPrimitives/SensorNotificationRequest.swift
+++ Sources/PakuShared/NetworkPrimitives/SensorNotificationRequest.swift
@@ -7,10 +7,24 @@
 
 import Foundation
 
-struct SensorNotificationRequest: Codable {
-    var userID: UUID
-    var sensorID: Int
-    var threshold: Int
-    var conversion: AQIConversion
-    var averagingPeriod: AverageTimePeriod
+public struct SensorNotificationRequest: Codable {
+    public var userID: UUID
+    public var sensorID: Int
+    public var threshold: Int
+    public var conversion: AQIConversion
+    public var averagingPeriod: AverageTimePeriod
+
+    public init(
+        userID: UUID,
+        sensorID: Int,
+        threshold: Int,
+        conversion: AQIConversion,
+        averagingPeriod: AverageTimePeriod
+    ) {
+        self.userID = userID
+        self.sensorID = sensorID
+        self.threshold = threshold
+        self.conversion = conversion
+        self.averagingPeriod = averagingPeriod
+    }
 }
