20f09579601e9fbd98551f591008c8bfb5afc7de
Kyle Bashour <kylebshr@me.com>
2023-01-03 12:38:33 -0800

    Init

--- Sources/PakuShared/SensorNotificationResponse.swift
+++ Sources/PakuShared/SensorNotificationResponse.swift
@@ -7,4 +7,20 @@ public struct SensorNotificationResponse: Codable {
     public var threshold: Int
     public var conversion: AQIConversion
     public var averagingPeriod: AverageTimePeriod
+
+    public init(
+        id: UUID,
+        sensorID: Int,
+        sensorName: String,
+        threshold: Int,
+        conversion: AQIConversion,
+        averagingPeriod: AverageTimePeriod
+    ) {
+        self.id = id
+        self.sensorID = sensorID
+        self.sensorName = sensorName
+        self.threshold = threshold
+        self.conversion = conversion
+        self.averagingPeriod = averagingPeriod
+    }
 }
