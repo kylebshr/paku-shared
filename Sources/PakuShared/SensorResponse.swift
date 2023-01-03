6c29f946ec5d09e9c37e753edb4dccb20c9b2dc7
Kyle Bashour <kylebshr@me.com>
2023-01-03 12:02:14 -0800

    Public init

--- Sources/PakuShared/SensorResponse.swift
+++ Sources/PakuShared/SensorResponse.swift
@@ -19,4 +19,44 @@ public struct SensorResponse: Codable {
     public let pm2_5_6hour: Double?
     public let pm2_5_24hour: Double?
     public let pm2_5_1week: Double?
+
+    public init(
+        id: Int,
+        name: String,
+        latitude: Double,
+        longitude: Double,
+        locationType: LocationType,
+        lastSeen: Date,
+        altitude: Double? = nil,
+        humidity: Double? = nil,
+        confidence: Int? = nil,
+        temperature: Double? = nil,
+        pm2_5: Double? = nil,
+        pm2_5_cf_1: Double? = nil,
+        pm2_5_10minute: Double? = nil,
+        pm2_5_30minute: Double? = nil,
+        pm2_5_60minute: Double? = nil,
+        pm2_5_6hour: Double? = nil,
+        pm2_5_24hour: Double? = nil,
+        pm2_5_1week: Double? = nil
+    ) {
+        self.id = id
+        self.name = name
+        self.latitude = latitude
+        self.longitude = longitude
+        self.locationType = locationType
+        self.lastSeen = lastSeen
+        self.altitude = altitude
+        self.humidity = humidity
+        self.confidence = confidence
+        self.temperature = temperature
+        self.pm2_5 = pm2_5
+        self.pm2_5_cf_1 = pm2_5_cf_1
+        self.pm2_5_10minute = pm2_5_10minute
+        self.pm2_5_30minute = pm2_5_30minute
+        self.pm2_5_60minute = pm2_5_60minute
+        self.pm2_5_6hour = pm2_5_6hour
+        self.pm2_5_24hour = pm2_5_24hour
+        self.pm2_5_1week = pm2_5_1week
+    }
 }
