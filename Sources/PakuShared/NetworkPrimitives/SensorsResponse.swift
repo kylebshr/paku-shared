150490a3380c590b24d609699db805619e9d709c
Kyle Bashour <kylebshr@me.com>
2023-01-11 10:52:10 -0800

    Add init

--- Sources/PakuShared/NetworkPrimitives/SensorsResponse.swift
+++ Sources/PakuShared/NetworkPrimitives/SensorsResponse.swift
@@ -2,4 +2,8 @@ import Foundation
 
 public struct SensorsResponse: Codable {
     public var sensors: [SensorResponse]
+
+    public init(sensors: [SensorResponse]) {
+        self.sensors = sensors
+    }
 }
