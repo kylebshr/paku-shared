f9a1bf78caef1eab6c7c8e0bb2f04d28e4bf60b6
Kyle Bashour <kylebshr@me.com>
2023-03-02 13:10:55 -0800

    Location

--- Sources/PakuShared/NetworkPrimitives/PrivateSensorResponse.swift
+++ Sources/PakuShared/NetworkPrimitives/PrivateSensorResponse.swift
@@ -5,11 +5,15 @@ public struct PrivateSensorResponse: Codable {
     public var name: String
     public let sensorID: Int
     public let sensorHardwareID: String
+    public let latitude: Double
+    public let longitude: Double
 
-    public init(id: UUID, name: String, sensorID: Int, sensorHardwareID: String) {
+    public init(id: UUID, name: String, sensorID: Int, sensorHardwareID: String, latitude: Double, longitude: Double) {
         self.id = id
         self.name = name
         self.sensorID = sensorID
         self.sensorHardwareID = sensorHardwareID
+        self.latitude = latitude
+        self.longitude = longitude
     }
 }
