25febd49b085c8d58e543f62889e9da5853a92d2
Kyle Bashour <kylebshr@me.com>
2023-03-07 18:19:31 -0800

    Add location type

--- Sources/PakuShared/NetworkPrimitives/PrivateSensorResponse.swift
+++ Sources/PakuShared/NetworkPrimitives/PrivateSensorResponse.swift
@@ -7,13 +7,15 @@ public struct PrivateSensorResponse: Codable {
     public let sensorHardwareID: String
     public let latitude: Double
     public let longitude: Double
+    public let locationType: LocationType
 
-    public init(id: UUID, name: String, sensorID: Int, sensorHardwareID: String, latitude: Double, longitude: Double) {
+    public init(id: UUID, name: String, sensorID: Int, sensorHardwareID: String, latitude: Double, longitude: Double, locationType: LocationType) {
         self.id = id
         self.name = name
         self.sensorID = sensorID
         self.sensorHardwareID = sensorHardwareID
         self.latitude = latitude
         self.longitude = longitude
+        self.locationType = locationType
     }
 }
