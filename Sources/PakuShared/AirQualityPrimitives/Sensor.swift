9eef96219cda440c6e9bf650abfbd8eb64d50050
Kyle Bashour <kylebshr@me.com>
2023-06-18 08:36:42 -0400

    Update EPA calc

--- Sources/PakuShared/AirQualityPrimitives/Sensor.swift
+++ Sources/PakuShared/AirQualityPrimitives/Sensor.swift
@@ -108,7 +108,44 @@ public struct Sensor: Codable, Equatable, Identifiable {
     }
 
     private func epaAQI(pm2_5: Double) -> Double {
-        aqiFrom(pm: (0.534 * pm2_5) - (0.0844 * Double(humidity ?? 35)) + 5.604).aqiClamped()
+        let e = Double(humidity ?? 25)
+        let t = pm2_5
+
+        func t260() -> Double {
+            2.966 + 0.69 * t + 8.84 * pow(10, -4) * pow(t, 2)
+        }
+
+        func t210() -> Double {
+            (0.69 * (t / 50 - 4.2) + 0.786 * (1 - (t / 50 - 4.2))) * t - 0.0862 * e * (1 - (t / 50 - 4.2)) + 2.966 * (t / 50 - 4.2) + 5.75 * (1 - (t / 50 - 4.2)) + 8.84 * pow(10, -4) * pow(t, 2) * (t / 50 - 4.2)
+        }
+
+        func t50() -> Double {
+            0.786 * t - 0.0862 * e + 5.75
+        }
+
+        func t30() -> Double {
+            (0.786 * (t / 20 - 1.5) + 0.524 * (1 - (t / 20 - 1.5))) * t - 0.0862 * e + 5.75
+        }
+
+        func d() -> Double {
+            0.524 * t - 0.0862 * e + 5.75
+        }
+
+        func correctedPM() -> Double {
+            if t >= 260 {
+                return t260()
+            } else if t >= 210 {
+                return t210()
+            } else if t >= 50 {
+                return t50()
+            } else if t >= 30 {
+                return t30()
+            } else {
+                return d()
+            }
+        }
+
+        return aqiFrom(pm: correctedPM()).aqiClamped()
     }
 
     private func aqiFrom(pm: Double) -> Double {
