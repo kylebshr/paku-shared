26df1be82907d6ed34c3c956e11e7dfc34f5091b
Kyle Bashour <kylebshr@me.com>
2025-06-25 09:47:42 -0700

    Make it public

--- Sources/PakuShared/NetworkPrimitives/NearestSensorSubscriptionStatus.swift
+++ Sources/PakuShared/NetworkPrimitives/NearestSensorSubscriptionStatus.swift
@@ -3,7 +3,7 @@ import Foundation
 public struct NearestSensorSubscriptionStatus: Codable, Sendable {
     public var subscription: NearestSensorSubscriptionResponse?
 
-    init(subscription: NearestSensorSubscriptionResponse?) {
+    public init(subscription: NearestSensorSubscriptionResponse?) {
         self.subscription = subscription
     }
 }
